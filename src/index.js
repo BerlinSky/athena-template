0bd7ca5703ff94444e25e0b192f9e0478acdf5aa
Berlin Sky <pedroyoung8989@gmail.com>
2016-11-29 08:56:31 -0800

    ES Lint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './main.css';
-
 import $ from 'jquery';
+
+/* eslint-disable no-console */
 
 $(function() {
   console.log("bundle works just fine.");
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 
+/* eslint-disable no-console */
+
 $(function () {
   console.log("main");
 });
