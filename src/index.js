397ccc69b28691fcfa327c8380649ee2e3360253
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-07 12:41:17 -0800

    set up JSON data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import './sass/main.scss';
 import $ from 'jquery';
 
+import {getUsers} from './services/data-service';
+
+
 /* eslint-disable no-console */
 
 $(function() {
   console.log("bundle works just fine.");
+});
+
+
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
 });
