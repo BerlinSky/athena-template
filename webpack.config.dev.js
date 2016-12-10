2b84272781f8d7d6b97d491e5c408f5b96c66554
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-10 09:17:27 -0800

    Add favicon

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,6 +16,21 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+    new FaviconsWebpackPlugin({
+      logo: './src/favicon.png',
+      icons: {
+      android: false,
+      appleIcon: false,
+      appleStartup: false,
+      coast: false,
+      favicons: true,
+      firefox: false,
+      opengraph: false,
+      twitter: false,
+      yandex: false,
+      windows: false
+    }
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true
