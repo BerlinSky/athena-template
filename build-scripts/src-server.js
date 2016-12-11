a2f64092a0add91d85e5b435cc2e582c73e863f2
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-11 11:44:58 -0800

    Update configuration

--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -15,6 +15,9 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.use('/static', express.static(path.join(__dirname, '../public')));
+// app.use('static', express.static(path.join(__dirname, '../src')));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
