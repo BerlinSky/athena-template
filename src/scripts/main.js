6e3c46036794c55fea07108550e904e6ce8d5538
Berlin Sky <pedroyoung8989@gmail.com>
2016-12-14 10:14:43 -0800

    search form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -80,35 +80,6 @@ menuClose.addEventListener('click', closeMenu);
 
 // $(function () {
 
-  // $('[data-mobileMenu="open"]').click(function() {
-  //   const mobileMenu = $('[data-mobileMenu="panel"]');
-
-  //   mobileMenu.css({'display': 'block'});
-  //   mobileMenu.css({right: '-320px'});
-  //   mobileMenu.addClass('magictime slideLeft');
-
-  //   setTimeout(function(){
-  //     mobileMenu.css({right: 0});
-  //     mobileMenu.removeClass('slideLeft');
-  //   }, 1000);
-
-  //   $('html, body').on('touchmove', function(evt){
-  //      evt.preventDefault();
-  //   });
-  // })
-
-  // $('[data-mobileMenu="close"]').click(function() {
-  //   $('html, body').unbind('touchmove');
-
-  //   const mobileMenu = $('[data-mobileMenu="panel"]');
-
-  //   mobileMenu.addClass('magictime slideRight');
-  //   setTimeout(function(){
-  //     mobileMenu.css({'display': 'none'});
-  //     mobileMenu.removeClass('slideRight');
-  //   }, 1000);
-  // })
-
 // });
 
 
