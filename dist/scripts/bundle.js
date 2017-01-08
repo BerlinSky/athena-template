199e85bf3feae0755617ec2ae71e21b7fe6a6809
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-08 01:45:16 -0800

    Update

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9831,6 +9831,35 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     var mobileMenu = (0, _jquery2.default)('.js-mobileMenuContainer');
     mobileMenu.toggle("slow");
   });
+
+  var menuWrapper = document.querySelector('.js-menuWrapper');
+  var showSearchPanel = document.querySelector('.js-showSearchPanel');
+
+  showSearchPanel.addEventListener('click', function (e) {
+    console.log(showSearchPanel);
+
+    e.preventDefault();
+    menuWrapper.classList.add('showSearchPanel');
+  });
+
+  // const showSearchPanel = $('.js-showSearchPanel');
+  // const searchPanel = $('.js-searchPanel');
+  // showSearchPanel.click(function(e) {
+  //   e.preventDefault();
+
+  //   showSearchPanel.hide();
+  //   searchPanel.show();
+  // })
+
+  var hideSearchPanel = document.querySelector('.js-hideSearchPanel');
+  // const hideSearchPanel = $('.js-hideSearchPanel');
+  hideSearchPanel.addEventListener('click', function (e) {
+    e.preventDefault();
+    // searchPanel.hide();
+    // showSearchPanel.show();
+
+    menuWrapper.classList.remove('showSearchPanel');
+  });
 });
 
 },{"jquery":1}]},{},[2])
