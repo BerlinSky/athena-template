df6fec838df1382765f8ee4aa6bb767437414713
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-08 02:30:36 -0800

    Update

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9833,30 +9833,17 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
   });
 
   var menuWrapper = document.querySelector('.js-menuWrapper');
+
   var showSearchPanel = document.querySelector('.js-showSearchPanel');
-
   showSearchPanel.addEventListener('click', function (e) {
-    console.log(showSearchPanel);
-
     e.preventDefault();
+
     menuWrapper.classList.add('showSearchPanel');
   });
 
-  // const showSearchPanel = $('.js-showSearchPanel');
-  // const searchPanel = $('.js-searchPanel');
-  // showSearchPanel.click(function(e) {
-  //   e.preventDefault();
-
-  //   showSearchPanel.hide();
-  //   searchPanel.show();
-  // })
-
   var hideSearchPanel = document.querySelector('.js-hideSearchPanel');
-  // const hideSearchPanel = $('.js-hideSearchPanel');
   hideSearchPanel.addEventListener('click', function (e) {
     e.preventDefault();
-    // searchPanel.hide();
-    // showSearchPanel.show();
 
     menuWrapper.classList.remove('showSearchPanel');
   });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,33 +10,19 @@ $(function () {
   })
 
   const menuWrapper = document.querySelector('.js-menuWrapper');
+
   const showSearchPanel = document.querySelector('.js-showSearchPanel');
-
   showSearchPanel.addEventListener('click', (e) => {
-    console.log(showSearchPanel);
-
     e.preventDefault();
+
     menuWrapper.classList.add('showSearchPanel');
   })
 
-  // const showSearchPanel = $('.js-showSearchPanel');
-  // const searchPanel = $('.js-searchPanel');
-  // showSearchPanel.click(function(e) {
-  //   e.preventDefault();
-
-  //   showSearchPanel.hide();
-  //   searchPanel.show();
-  // })
-
   const hideSearchPanel = document.querySelector('.js-hideSearchPanel');
-  // const hideSearchPanel = $('.js-hideSearchPanel');
   hideSearchPanel.addEventListener('click', (e) => {
     e.preventDefault();
-    // searchPanel.hide();
-    // showSearchPanel.show();
 
     menuWrapper.classList.remove('showSearchPanel');
   })
-
 
 });
