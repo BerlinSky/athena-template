51730704aa8820234cd639744c408f4387fa2fd4
Berlin Sky <pedroyoung8989@gmail.com>
2017-01-09 19:33:25 -0800

    Update

--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -27,6 +27,36 @@ app.get('/menus', function(req, res) {
   ])
 });
 
+app.get('/menus2', function(req, res) {
+  res.json([
+    {"id": 1, "name":"Books2", "description":"A list of books", "link":"http://google.com"},
+    {"id": 2, "name":"Author2", "description":"A list of authors", "link":"http://google.com"},
+    {"id": 3, "name":"Club2", "description":"A list of clubs", "link":"http://google.com"},
+    {"id": 4, "name":"Books2", "description":"A list of books", "link":"http://google.com"},
+    {"id": 5, "name":"Author2", "description":"A list of authors", "link":"http://google.com"},
+    {"id": 6, "name":"Club2", "description":"A list of clubs", "link":"http://google.com"},
+    {"id": 7, "name":"Books2", "description":"A list of books", "link":"http://google.com"},
+    {"id": 8, "name":"Author2", "description":"A list of authors", "link":"http://google.com"},
+    {"id": 9, "name":"Club2", "description":"A list of clubs", "link":"http://google.com"}
+  ])
+});
+
+app.get('/books', function(req, res) {
+  res.json([
+    { title: 'Pride and Prejudice', author: 'Jane Austen', id: 1001 },
+    { title: 'Little Women', author: 'Louisa May Alcott', id: 1002 },
+    { title: 'The Secret Garden', author: 'Frances Hodgson Burnett', id: 1003 }
+  ])
+});
+
+app.get('/authors', function(req, res) {
+  res.json([
+    { name: 'Jane Austen', twitter: '@jauten', bio: 'British literary icon' },
+    { name: 'Louisa May Alcott', twitter: '@l.m.alcott', bio: 'The famouts author for classic children books' },
+    { name: 'Frances Hodgson Burnett', twitter: '@fhburnett', bio: 'The author for the secret garden' }
+  ])
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import './sass/main.scss';
 
 /* eslint-disable no-console */
 
+/*
+// Consume Promise:
+*/
 const menuListPromise = fetch('http://localhost:3010/menus');
-// console.log(menuListPromise);
-
 menuListPromise
   .then(data => data.json())
   .then(data => { console.log(data) })
@@ -12,3 +13,103 @@ menuListPromise
     console.error(err);
   })
 
+/*
+// Create Promise:
+*/
+const promised = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve('The data requested.');
+    reject(Error('Err occured, unfortunately'));
+  }, 1000);
+});
+
+promised
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+/*
+// Handle multiple Promises
+// Note: it waits for the longest-running response.
+*/
+const menuUrl = 'http://localhost:3010/menus';
+const menu2Url = 'http://localhost:3010/menus2';
+
+const menuPromise = fetch(menuUrl);
+const menu2Promise = fetch(menu2Url);
+
+Promise
+  .all([menuPromise, menu2Promise])
+  .then(responses => {
+    return Promise.all(responses.map(res => res.json()))
+  })
+  .then(responses => {
+    console.log(responses);
+
+    console.table(responses[0]);
+    console.table(responses[1]);
+  });
+
+/*
+// How to chain Promises
+*/ 
+function getBookById(bookId) {
+  return new Promise((resolve, reject) => {
+    const bookUrl = 'http://localhost:3010/books';
+
+    setTimeout(() => {
+      const booksPromise = fetch(bookUrl);
+
+      booksPromise
+        .then(data => data.json())
+        .then(data => {
+          const book = data.find(book => book.id === bookId);
+          console.log(book);
+          if (book) {
+            resolve(book);
+          }
+          else {
+            reject(Error("No books are found."));
+          }
+        })
+    }, 1000);
+
+  })
+}
+
+function getAuthorForBook(book) {
+  return new Promise((resolve, reject) => {
+    const authorUrl = 'http://localhost:3010/authors';
+    
+    setTimeout(() => {
+      const authorPromise = fetch(authorUrl);
+
+      authorPromise
+        .then(data => data.json())
+        .then(data => {
+          const author = data.find(author => author.name === book.author);
+          if (author) {
+            book.author = author;
+            resolve(book);
+          }
+          else {
+            reject(Error('No authors are found'));
+          }
+        })
+    } ,500)
+  })
+}
+
+getBookById(1001)
+  .then(book => {
+    return getAuthorForBook(book);
+  })
+  .then(author => {
+    console.log(author);
+  })
+  .catch(err => {
+    console.error(err);
+  })
