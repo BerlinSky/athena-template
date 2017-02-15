61ef460a73efbfc776d445fa618e407530989bad
Berlin Sky <pedroyoung8989@gmail.com>
2017-02-15 08:22:38 -0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
 import './main.css';
 import $ from 'jquery';
 
+import { curry } from "lodash";
+
+// import { addEvent } from './scripts/event';
+
 /* eslint-disable no-console */
 
 $(function() {
   console.log("bundle works just fine.");
+
+  $('.eventCreator').click(function() {
+    // disable default event behavior
+    // e.preventDefault();
+
+    $('body').trigger("custom:changed");
+
+    console.log("fired the custom event");
+
+    $(window).trigger("window:changed");
+    
+  })
+
+  $('body').on('custom:changed', function(e) {
+    e.preventDefault();
+
+    console.log("received the custom event");
+  })
+
+  // addEvent(window, 'storage:changed', function (event) {
+  //   console.log('window event created', event);
+  // });
+
+  $(window).on('window:changed', function() {
+
+    console.log("received the custom event from window");
+  })
 });
+
+var addFourNumbers = (a, b, c, d) => a + b + c + d;
+
+var curriedAddFourNumbers = curry(addFourNumbers);
+var f = curriedAddFourNumbers(5, 2);
+var g = f(3);
+var x = g(4);
+
+console.log(x);
+
+let dataStructure = {
+  "enterprise": "Golden Apple",
+  "band": {
+    "manager": "Joe Scarface",
+    "leadSinger": "Bluefish",
+    "dancers": [
+      "speddy",
+      "snake",
+      "braves"
+    ]
+  }
+}
+
+console.log(dataStructure);
---
+++ src/scripts/event.js
@@ -0,0 +1,24 @@
+export function addEvent() {
+  if (document.addEventListener) {
+    return function (el, type, fn) {
+      if (el.length) {
+        for (var i = 0; i < el.length; i++) {
+          addEvent(el[i], type, fn);
+        }
+      } else {
+        el.addEventListener(type, fn, false);        
+      }
+    };
+  } 
+  else {
+    return function (el, type, fn) {
+      if (el.length) {
+        for (var i = 0; i < el.length; i++) {
+          addEvent(el[i], type, fn);
+        }
+      } else {
+        el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
+      }
+    };
+  }
+}
