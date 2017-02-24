ee650c686950495c3c6b9b6112270b2718a1841b
Berlin Sky <pedroyoung8989@gmail.com>
2017-02-23 17:35:17 -0800

    up

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -12693,12 +12693,19 @@ imagesLoaded.makeJQueryPlugin(_jquery2.default);
 
 function paintMasonryLayout() {
   var $container = (0, _jquery2.default)('.js-imageGridContainer');
+  var griderSizer = (0, _jquery2.default)('.js-gridSizer');
+
+  console.log('gutter', griderSizer.attr('data-grid-gutter'));
 
   if ($container) {
+    var gutterSize = parseInt(griderSizer.attr('data-grid-gutter')) || 0;
+    // const gutterSize = 10;
+
     var $imageContainer = $container.masonry({
       itemSelector: '.js-grid',
       percentPosition: true,
-      columnWidth: '.js-gridSizer'
+      columnWidth: '.js-gridSizer',
+      gutter: gutterSize
     });
 
     // export
--- src/scripts/masonry-layout.js
+++ src/scripts/masonry-layout.js
@@ -9,12 +9,19 @@ imagesLoaded.makeJQueryPlugin( $ );
 
 export function paintMasonryLayout() {
   const $container = $('.js-imageGridContainer');
+  const griderSizer = $('.js-gridSizer');
+
+  console.log('gutter', griderSizer.attr('data-grid-gutter'));
 
   if ($container) {
+    const gutterSize = parseInt(griderSizer.attr('data-grid-gutter')) || 0;
+    // const gutterSize = 10;
+
     const $imageContainer = $container.masonry({
         itemSelector: '.js-grid',
         percentPosition: true,
-        columnWidth: '.js-gridSizer'
+        columnWidth: '.js-gridSizer',
+        gutter: gutterSize
       });
 
     // export
