b33e7350a1d47d5b36b2615d04938ba0debe97b4
Berlin Sky <pedroyoung8989@gmail.com>
2017-04-18 16:59:36 -0700

    Refactor JS modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,10 @@
 import $ from 'jquery';
-var Masonry = require('masonry-layout');
-var jQueryBridget = require('jquery-bridget');
-
-jQueryBridget( 'masonry', Masonry, $ );
+import { initMobileMenu } from './mobile-menu';
+import { paintMasonryLayout } from './masonry-layout';
+import { initFancyInputBox } from './fancy-input';
 
 $(function () {
-
-  $('.js-toggleMobileMenu').click(function(e) {
-    e.preventDefault();
-
-    const mobileMenu = $('.js-mobileMenuContainer');
-    mobileMenu.toggleClass("js-l-mobile");
-  })
-
-  const menuWrapper = document.querySelector('.js-menuWrapper');
-
-  const showSearchPanel = document.querySelector('.js-showSearchPanel');
-  showSearchPanel.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    menuWrapper.classList.add('showSearchPanel');
-  })
-
-  const hideSearchPanel = document.querySelector('.js-hideSearchPanel');
-  hideSearchPanel.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    menuWrapper.classList.remove('showSearchPanel');
-  })
-
-  const $grid = $('.js-imageGridContainer').masonry({
-    itemSelector: '.js-grid',
-    percentPosition: true,
-    columnWidth: '.js-gridSizer'
-  });
-
-  $grid.masonry();
+  initMobileMenu();
+  paintMasonryLayout();
+  initFancyInputBox();
 });
-
-// import $ from 'jquery';
-// import { initMobileMenu } from './mobile-menu';
-// import { paintMasonryLayout } from './masonry-layout';
-// import { initFancyInputBox } from './fancy-input';
-
-// $(function () {
-//   initMobileMenu();
-//   paintMasonryLayout();
-//   initFancyInputBox();
-// });
--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -6,36 +6,23 @@ export function initMobileMenu() {
     e.preventDefault();
 
     const mobileMenu = $('.js-mobileMenuContainer');
-    mobileMenu.toggle("slow");
+    mobileMenu.toggleClass("js-l-mobile");
   })
 
-  const menuWrapper = $('.js-menuWrapper');
-  const showSearchPanel = $('.js-showSearchPanel');
+  const menuWrapper = document.querySelector('.js-menuWrapper');
 
-  showSearchPanel.click(function(e) {
+  const showSearchPanel = document.querySelector('.js-showSearchPanel');
+  showSearchPanel.addEventListener('click', (e) => {
     e.preventDefault();
 
-    menuWrapper.addClass('showSearchPanel');
+    menuWrapper.classList.add('showSearchPanel');
   })
 
-  // showSearchPanel.addEventListener('click', (e) => {
-  //   e.preventDefault();
-
-  //   menuWrapper.classList.add('showSearchPanel');
-  // })
-
-  const hideSearchPanel = $('.js-hideSearchPanel');
-  hideSearchPanel.click(function(e) {
+  const hideSearchPanel = document.querySelector('.js-hideSearchPanel');
+  hideSearchPanel.addEventListener('click', (e) => {
     e.preventDefault();
 
-    menuWrapper.removeClass('showSearchPanel');
+    menuWrapper.classList.remove('showSearchPanel');
   })
-
-  // const hideSearchPanel = document.querySelector('.js-hideSearchPanel');
-  // hideSearchPanel.addEventListener('click', (e) => {
-  //   e.preventDefault();
-
-  //   menuWrapper.classList.removeClass('showSearchPanel');
-  // })
 }
 
